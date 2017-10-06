d6a67680ac0fa8e04760671020a4c7ea0bcfda61
tiro <tiromodibedi39@gmail.com>
2017-10-06 16:04:21 +0200

    done

--- csc1016Assignments/assignment6/CowsAndBulls.java
+++ csc1016Assignments/assignment6/CowsAndBulls.java
@@ -7,41 +7,45 @@
 public class CowsAndBulls{
   // instance variables
   private NumberPicker numberPicker;
-  private int guessValue;
+  private int mysteryValue;
   private int guesses;
-
+  private int lastGuess;
   // constants
   public final static int NUM_DIGITS = 4;
   public final static int MAX_VALUE = 9876;
   public final static int MIN_VALUE = 1234;
   public final static int MAX_GUESSES = 10;
 
-  // contructors
+  // contructor
   public CowsAndBulls(int seed){
-    numberPicker = new NumberPicker(seed, MIN_VALUE, MAX_VALUE);
-    this.guessValue = numberPicker.nextInt();
+    numberPicker = new NumberPicker(seed, 1, 9);
     this.guesses = MAX_GUESSES;
+    this.mysteryValue = numberPicker.nextInt();
+    for (int i = 0; i < NUM_DIGITS - 1; i++) {
+       mysteryValue = mysteryValue*10 + numberPicker.nextInt();
+    }
   }
 
   public int guessesRemaining(){
     return guesses;
   }
 
-  public Result guess(int guessNumber){
-    int value = numberPicker.nextInt();
-    int bulls = NumberUtils.countMatches(guessNumber, value);
-    int cows = NumberUtils.countIntersect(guessNumber, value);
+  public Result guess(int myGuess){
+    int bulls = NumberUtils.countMatches(myGuess, mysteryValue);
+    int cows = NumberUtils.countIntersect(myGuess, mysteryValue);
     Result result = new Result(cows, bulls);
     guesses--;
+    lastGuess = myGuess;
     return result;
   }
 
   public int giveUp(){
-    return guessValue;
+    guesses = 0;
+    return mysteryValue;
   }
 
   public boolean gameOver(){
-    return guesses == 0;
+    return (guesses == 0 || (lastGuess == mysteryValue));
   }
 
 }
